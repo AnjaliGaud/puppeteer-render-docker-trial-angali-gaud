457363430a3efaa511ff30107c05886a9231bd8b
JitendraJSM <jitendranathswamijsm@gmail.com>
2024-05-23 19:41:00 +0530

    headless:false testing on render

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const scrapeLogic = async () => {
   const browser = await puppeteer.launch({
+    headless: false,
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
